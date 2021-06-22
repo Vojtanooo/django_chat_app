b9a786e1b77b9a7ce311f23209c86ce294ecdf8b
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-06-22 18:44:44 +0200

    create register view and template

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
-
+from chat_app.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from .models import UserProfile
+from .forms import UserForm
 # Create your views here.
 
 
-def index(request):
-    return render(request, "index.html")
+def register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Your Account has been created!")
+            return redirect("register")
+    else:
+        form = UserForm()
+
+    return render(request, "register.html", {"form": form})
 
 
 def room(request, room_name):
-    return render(request, "create_chat.html", {
-        "room_name": room_name
-    })
+    username = request.user
+    User = get_user_model()
+    users = User.objects.all()
+
+    return render(request, "create_chat.html", {"room_name": room_name, "username": username, "users": users})
