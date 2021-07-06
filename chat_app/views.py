6162db316c34f20583e05df161519206494c6777
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-07-06 18:59:17 +0200

    add login page and logic behind room page

--- chat_app/views.py
+++ chat_app/views.py
@@ -2,12 +2,11 @@ from chat_app.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib import messages
-from .models import UserProfile
+from django.contrib.auth import authenticate, login, logout
 from .forms import UserForm
-# Create your views here.
 
 
-def register(request):
+def register_page(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -20,9 +19,41 @@ def register(request):
     return render(request, "register.html", {"form": form})
 
 
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("room", room_name="Chat")
+        else:
+            messages.error(request, f"Username or Password is incorect!")
+
+    return render(request, "login.html")
+
+
 def room(request, room_name):
     username = request.user
     User = get_user_model()
     users = User.objects.all()
 
-    return render(request, "create_chat.html", {"room_name": room_name, "username": username, "users": users})
+    dict_of_users_href = {}
+    for user in users:
+        if not user == username:
+            dict_of_users_href[f"{user}"] = "chat{}_{}".format(
+                *sorted([username.id, user.id]))
+
+    room_user_list = str(room_name).strip("chat").split("_")
+    if str(username.id) in room_user_list:
+        room_user_list.remove(str(username.id))
+
+    room_user_name = User.objects.get(id=room_user_list[0])
+
+    return render(request, "create_chat.html", {
+        "room": dict_of_users_href,
+        "room_name": room_name,
+        "username": username, "users": users,
+        "room_user_name": room_user_name
+    })
