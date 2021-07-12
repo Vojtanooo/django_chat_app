55c3ce8d9d2e5912c65f5345ca3007f7229de0ba
Vojtěch Janeček <janecek.vojta@gmail.com>
2021-07-12 14:09:49 +0200

    create profile view and template

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,9 +1,11 @@
-from chat_app.models import User
+from chat_app.models import User, UserProfile
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from .forms import UserForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
+from .forms import UserForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register_page(request):
@@ -27,13 +29,14 @@ def login_page(request):
 
         if user is not None:
             login(request, user)
-            return redirect("room", room_name="Chat")
+            return redirect("room", room_name="Welcome")
         else:
             messages.error(request, f"Username or Password is incorect!")
 
     return render(request, "login.html")
 
 
+@login_required()
 def room(request, room_name):
     username = request.user
     User = get_user_model()
@@ -42,14 +45,17 @@ def room(request, room_name):
     dict_of_users_href = {}
     for user in users:
         if not user == username:
-            dict_of_users_href[f"{user}"] = "chat{}_{}".format(
+            dict_of_users_href[user] = "chat{}_{}".format(
                 *sorted([username.id, user.id]))
 
-    room_user_list = str(room_name).strip("chat").split("_")
-    if str(username.id) in room_user_list:
-        room_user_list.remove(str(username.id))
+    if room_name == "Welcome":
+        room_user_name = "Welcome"
+    else:
+        room_user_list = str(room_name).strip("chat").split("_")
+        if str(username.id) in room_user_list:
+            room_user_list.remove(str(username.id))
 
-    room_user_name = User.objects.get(id=room_user_list[0])
+        room_user_name = User.objects.get(id=room_user_list[0])
 
     return render(request, "create_chat.html", {
         "room": dict_of_users_href,
@@ -57,3 +63,29 @@ def room(request, room_name):
         "username": username, "users": users,
         "room_user_name": room_user_name
     })
+
+
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.userprofile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Yout Account has been updated!")
+            return redirect("room", room_name="Welcome")
+        else:
+            print(u_form.errors.as_data())
+            print(p_form.errors.as_data())
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.userprofile)
+
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+
+    return render(request, "profile.html", context)
